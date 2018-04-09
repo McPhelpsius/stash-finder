39ad61a48da3586c0389094cbf8057ab9998846f
Nathan Phelps <McPhelpsius@Nathans-MBP-2.local>
2018-04-09 14:54:47 -0500

    Create Stash GQL, with proper params Display and behavior in Admin container/component Proper parameters for mutation

--- src/containers/Admin.js
+++ src/containers/Admin.js
@@ -1,30 +1,69 @@
 import React, { Component } from 'react';
-import { withApollo, compose, graphql } from 'react-apollo';
+import { withApollo } from 'react-apollo';
 import createStash from '../gql/createStash';
 import getAllStashes from '../gql/getAllStashes';
 import AdminComponent from '../components/Admin';
 
 class AdminContainer extends Component {
-  state = {
-    editing: false,
-    name: '',
-    lat: '',
-    lng: '',
-    claimCode: '',
-    clue1: '',
-    clue2: '',
-    claimed: '',
+  constructor() {
+    super();
+    this.state = {
+      editing: false,
+      error: '',
+      status: '',
+      stashes: [],
+      name: '',
+      lat: 0,
+      lng: 0,
+      claimCode: '',
+      clue1: '',
+      clue2: '',
+      claimed: '',
+      mapCenter: { lat: 39.031114, lng: -94.593063 },
+      mapZoom: 15,
+    };
+  }
+
+  componentDidMount() {
+    this.queryAllBalls();
+  }
+
+  queryAllBalls = async () => {
+    try {
+      const allResponse = await this.props.client.query({
+        query: getAllStashes,
+        fetchPolicy: 'no-cache',
+      });
+
+      this.setState({
+        stashes: allResponse.data.stashes,
+      });
+    } catch (e) {
+      this.setState({ error: "Couldn't query all the stashes" });
+      console.log(e);
+    }
+  };
+
+  recenterMap = (lat, lng) => {
+    this.setState({ mapCenter: { lat, lng }, mapZoom: 18 });
+    window.scrollTo(0, 0);
+  };
+
+  updateValue = (key, value) => {
+    this.setState({ [key]: value });
   };
 
   addStash = async () => {
     const { name, lat, lng, claimCode, clue1, clue2, claimed } = this.state;
     try {
-      const response = this.props.client.mutate({
+      const lati = parseFloat(lat, 10);
+      const long = parseFloat(lng, 10);
+      const response = await this.props.client.mutate({
         mutation: createStash,
         variables: {
           name,
-          lat,
-          lng,
+          lat: lati,
+          lng: long,
           claimCode,
           clue1,
           clue2,
@@ -32,31 +71,55 @@ class AdminContainer extends Component {
         },
       });
 
-      console.log(response);
+      this.setState({
+        status: `Stash ${response.data.createStash.name} successfully added`,
+        error: '',
+      });
+
+      this.queryAllBalls();
     } catch (e) {
+      this.setState({ status: '', error: e.message });
       console.log('error: ', e);
     }
   };
 
   render() {
+    const {
+      editing,
+      error,
+      status,
+      name,
+      lat,
+      lng,
+      claimCode,
+      clue1,
+      clue2,
+      claimed,
+      stashes,
+      mapCenter,
+      mapZoom,
+    } = this.state;
     return (
       <AdminComponent
-        editing={this.state.editing}
-        name={this.state.name}
-        lat={this.state.lat}
-        lng={this.state.lng}
-        claimCode={this.state.claimCode}
-        clue1={this.state.clue1}
-        clue2={this.state.clue2}
-        claimed={this.state.claimed}
+        editing={editing}
+        error={error}
+        status={status}
+        name={name}
+        lat={lat}
+        lng={lng}
+        claimCode={claimCode}
+        clue1={clue1}
+        clue2={clue2}
+        claimed={claimed}
+        updateValue={this.updateValue}
         addStash={this.addStash}
-        stashes={this.props.allStashes.stashes || []}
+        stashes={stashes || []}
+        recenterMap={this.recenterMap}
+        mapCenter={mapCenter}
+        mapZoom={mapZoom}
       />
     );
   }
 }
 
-export default compose(
-  withApollo,
-  graphql(getAllStashes, { name: 'allStashes' }),
-)(AdminContainer);
+export default withApollo(AdminContainer);
