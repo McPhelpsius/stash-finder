11ac133d2542b6f9e22efbc1f3ff2a6dbc8ea62a
Nathan Phelps <McPhelpsius@Nathans-MBP-2.home>
2018-04-11 23:41:15 -0500

    Server now has function and resolver for claiming stashes Users can only claim the closest stash Server queries for stash by closest stash id and then checks that the code is right for that one User can submit a claim code on button press

--- server/src/Mutation/stash.js
+++ server/src/Mutation/stash.js
@@ -1,43 +1,69 @@
-module.exports = {
-  createStash(parent, { name, lat, lng, claimCode, clue1, clue2 }, ctx, info) {
-    return ctx.db.mutation.createStash(
-      {
-        data: {
-          name,
-          lat,
-          lng,
-          claimCode,
-          clue1,
-          clue2,
-          claimed: false,
-        },
+function createStash(
+  parent,
+  { name, lat, lng, claimCode, clue1, clue2 },
+  ctx,
+  info,
+) {
+  return ctx.db.mutation.createStash(
+    {
+      data: {
+        name,
+        lat,
+        lng,
+        claimCode,
+        clue1,
+        clue2,
+        claimed: false,
       },
-      info,
-    );
-  },
-  deleteStash(parent, { id }, ctx, info) {
-    return ctx.db.mutation.deleteStash({ where: { id } }, info);
-  },
-  updateStash(
-    parent,
-    { id, name, lat, lng, claimCode, clue1, clue2, claimed },
-    ctx,
+    },
     info,
-  ) {
+  );
+}
+function deleteStash(parent, { id }, ctx, info) {
+  return ctx.db.mutation.deleteStash({ where: { id } }, info);
+}
+
+function updateStash(
+  parent,
+  { id, name, lat, lng, claimCode, clue1, clue2, claimed },
+  ctx,
+  info,
+) {
+  return ctx.db.mutation.updateStash(
+    {
+      where: { id },
+      data: {
+        name,
+        lat,
+        lng,
+        claimCode,
+        clue1,
+        clue2,
+        claimed,
+      },
+    },
+    info,
+  );
+}
+
+async function claimStash(parent, { id, claimCode }, ctx, info) {
+  const stash = await ctx.db.query.stash({ where: { id } });
+  if (stash.claimCode === claimCode) {
     return ctx.db.mutation.updateStash(
       {
         where: { id },
-        data: {
-          name,
-          lat,
-          lng,
-          claimCode,
-          clue1,
-          clue2,
-          claimed,
-        },
+        data: { claimed: true },
       },
       info,
     );
-  },
+  }
+
+  return new Error('That claim code is wrong');
+}
+
+module.exports = {
+  createStash,
+  deleteStash,
+  updateStash,
+  claimStash,
 };
