7d5750fdec224cffc0d613989da9a5802e49e77a
Nathan Phelps <McPhelpsius@Nathans-MBP-2.home>
2018-05-14 16:51:09 -0500

    Updates

--- server/src/Mutation/index.js
+++
@@ -1,9 +0,0 @@
-const user = require('./user');
-const stash = require('./stash');
-const auth = require('./auth');
-
-module.exports = {
-  user,
-  stash,
-  auth,
-};
--- src/components/Claim.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Typography from 'material-ui/Typography';
-
-export default function Claim() {
-  return <Typography variant="display3">Claim</Typography>;
-}
